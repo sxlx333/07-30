d750383ca0d48bc27c7bbf4dbbb77bd7bf742b10
sxlx333 <knifeq8@gmail.com>
2025-08-24 14:54:01 +0300

    mobile nav and favicon

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.querySelector('.hamburger');
   const headerNav = document.querySelector('.headerNav');
+  const headerContainer = document.querySelector('.headerContainer');
+  const topLine = document.querySelector('.topLine');
 
   hamburger.addEventListener('click', () => {
-    headerNav.classList.toggle('active');
+    document.documentElement.classList.toggle('no-scroll');
     document.body.classList.toggle('no-scroll');
+    headerNav.classList.toggle('active');
+  });
+
+  const topLineHeight = topLine.offsetHeight;
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY >= topLineHeight) {
+      headerContainer.classList.add('fixed');
+    } else {
+      headerContainer.classList.remove('fixed');
+    }
   });
 });
